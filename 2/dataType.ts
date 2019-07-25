d3473042f11dc46d868822c617e524a0fb54f581
hcxowe <hcxowe@126.com>
2019-07-25 16:41:39 +0800

    基本类型

--- 2/dataType.ts
+++ 2/dataType.ts
@@ -1,70 +1,64 @@
-// 基本类型
-let bol: boolean = true
-let num: number = 1.234
-let na: string = 'hcxowe'
+// 布尔值
+let isDone: boolean = false
 
-let nums: number[] = [1, 2, 3]
-let chrs: string[] = ['1', '2', '3']
-let chrs2: Array<string> = ['xx', 'yy', 'zz']
+// 数值
+let num: number = 4
 
-let x: [string, number] = ['1', 2]
+// 字符串
+let str: string = 'hcxowe'
 
+// 数组
+let ary: number[] = [1, 2, 3, 4, 5]
+let ary1: string[] = ['1', '2', '3', '4', '5']
+let ary11: any[] = [1, false, 'hcxowe', {}]
+
+let ary2: Array<number> = [1, 2, 3, 4, 5]
+
+// 元组
+let x: [string, number, boolean]
+x = ['hello', 10, false]
+
+// 枚举
 enum Color {
-    Red,
-    Green,
+    Red = 0, 
+    Green, 
     Blue
 }
 
-let color: Color = Color.Red // color = 0
-let colorName: string = Color[1] // colorName = 'Green'
+let c: Color = Color.Green
 
-let any: any = 1
-any = 'hcxowe'
-any = true
-let anys: any[] = [1, 'x', false]
+// Any
+let notSure: any = 4
+notSure = "maybe a string instead"
+notSure = false
 
-function getSomething(): void {
-    console.log('nothing')
+// Void
+let unusable: void = undefined
+
+function warnUser(): void {
+    console.log("This is my warning message")
 }
 
-let undef: undefined = undefined
-let nul: null = null
+// Null, Undefined
+let u: undefined = undefined
+let n: null = null
 
-num = null
-num = undefined
+// Never
 
-let strlen: number = (any as string).length
-
-// 变量声明
-var a = 1
-let b = 2
-const c = 3
-
-let [f, g] = [1, 2]
-let {xx, yy} = {xx: 'x', yy: 'y'}
-let o = {...{xx: 'x', yy: 'y'}, zz: 'z'}
-
-// 接口
-interface LabelledValue {
-    readonly input?: number; // 只读
-    label: string;
-    color?: string;
-    list?: ReadonlyArray<number>; // 只读数组
-    [propName: string]: any; // 其他任性的属性
+// 返回never的函数必须存在无法达到的终点
+function error(message: string): never {
+    throw new Error(message)
 }
 
-function printLabel(labelledObj:LabelledValue) {
-    console.log(labelledObj.label)
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+    while (true) {}
 }
 
-printLabel({label: 'xyz'})
+// Object
+// object表示非原始类型，也就是除number，string，boolean，symbol，null或undefined之外的类型
 
-interface SearchFunc {
-    (source: string, subString: string): boolean;
-}
-
-let mySearch: SearchFunc;
-mySearch = function(source: string, subString: string):boolean {
-    let result = source.search(subString);
-    return result > -1;
-}
+// 类型断言
+let someValue: any = "this is a string"
+let strLength: number = (<string>someValue).length
+let strLength1: number = (someValue as string).length
